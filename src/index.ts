2f9a9067091e2b1ff6375f52987cd5d943c0ae4b
Mathss18 <theus.7@hotmail.com>
2023-04-09 20:47:30 -0300

    testing league-connect

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import { VirtualClient } from "./client/VirtualClient";
 import { Gamemode } from "./enums/gamemode.enum";
 import { Role } from "./enums/role.enum";
+import { authenticate } from "league-connect";
 
 class Main {
   async start() {
-    const virtualClient = new VirtualClient();
-    await virtualClient.login("Lannydh", "88121747m");
-    await virtualClient.createLobby();
-    await virtualClient.selectGamemode(Gamemode.SOLO_DUO);
-    await virtualClient.selectRoles([Role.FILL, Role.UNSELECTED]);
-    await virtualClient.startFindingMatch();
+    // const virtualClient = new VirtualClient();
+    // await virtualClient.login("Lannydh", "88121747m");
+    // await virtualClient.createLobby();
+    // await virtualClient.selectGamemode(Gamemode.SOLO_DUO);
+    // await virtualClient.selectRoles([Role.FILL, Role.UNSELECTED]);
+    // await virtualClient.startFindingMatch();
+
+    const credentials = await authenticate();
+    console.log(credentials); // { password: '37dn2gsxH3ns', port: 37241 }
   }
 }
 
